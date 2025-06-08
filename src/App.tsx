9d31456cb2959f4a4dbae81657b25a2118407782
InvelarSolutions <invelarsolutions@gmail.com>
2025-06-08 20:49:32 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ContactPage from './pages/ContactPage'
-import SetupPage from './pages/SetupPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<ContactPage />} />
-      <Route path="/setup" element={<SetupPage />} />
     </Routes>
   )
 }
